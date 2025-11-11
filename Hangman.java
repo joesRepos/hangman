bfdc2b3c4f107485d223df5252b4ceaa1a683d83
joesRepos <joemitchell1717@gmail.com>
2025-11-11 17:18:05 +0000

    Loops while guesses are inputted, exits when exit command is entered.

--- Hangman.java
+++ Hangman.java
@@ -1,8 +1,18 @@
 package hangman;
+import java.util.Scanner;
 
 public class Hangman {
+    
     public static void main(String[] args) {
         System.out.println("Hangman Game");
+        play();
     }
-    
+
+    public static void play() {
+        Scanner scanner = new Scanner(System.in);
+        String guess = "";
+        while (!guess.equals("EXIT")) {
+            guess = scanner.nextLine();
+        }
+    }
 }
