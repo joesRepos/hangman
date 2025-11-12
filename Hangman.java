189620225b3115d87dbbc3fa7e02663e986256cd
joesRepos <joemitchell1717@gmail.com>
2025-11-12 17:15:09 +0000

    Set up winning logic.

--- Hangman.java
+++ Hangman.java
@@ -6,13 +6,23 @@ public class Hangman {
     public static void main(String[] args) {
         System.out.println("Hangman Game");
         play();
+    }  
+     
+    public static String getAnswer() {
+        return "ANSWER";
     }
 
     public static void play() {
         Scanner scanner = new Scanner(System.in);
+        String answer = getAnswer();
         String guess = "";
         while (!guess.equals("EXIT")) {
             guess = scanner.nextLine();
+
+            if (guess.equals(answer)) {
+                System.out.println("WINNER");
+                return;
+            }
         }
     }
 }
