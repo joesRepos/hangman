5fa061552e699103e8ec43a1c4dfb8ca16648cc5
joesRepos <joemitchell1717@gmail.com>
2025-11-13 20:17:29 +0000

    Method for masking the answer.

--- Hangman.java
+++ Hangman.java
@@ -12,11 +12,17 @@ public class Hangman {
         return "ANSWER";
     }
 
+    public static String maskAnswer(String answer) {
+        return "*".repeat(answer.length());
+    }
+
     public static void play() {
         Scanner scanner = new Scanner(System.in);
         String answer = getAnswer();
+        String maskedAnswer = maskAnswer(answer);
         String guess = "";
         while (!guess.equals("EXIT")) {
+            System.out.println(maskedAnswer);
             guess = scanner.nextLine();
 
             if (guess.equals(answer)) {
